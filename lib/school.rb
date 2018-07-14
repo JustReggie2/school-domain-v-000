1a93198ce5fa6c82941d666cccf16de74a4e4446
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-07-14 21:26:18 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,11 @@
 class School
-  attr_accessor :name, :roster
-  def initialize(name)
-    @name = name
+  attr_accessor :roster
+
+  def initialize(school_name)
+    @school_name = school_name
     @roster = {}
+  end
+
+  def add_student()
   end
 end
